8514013486c1b6e9959fa0fa76fa21570ad4b444
carmen887 <00100619@uca.edu.sv>
2019-11-15 19:39:36 -0600

    Ejercicio 2 labo06

---
+++ Laboratorio/Laboratorio 06/Ejercicio 2.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+using namespace std;
+
+//------ Creacion de nodo y de arbol ------
+struct nodo{
+    int info;
+    struct nodo *izq;
+    struct nodo *der;
+};
+typedef struct nodo Nodo;
+typedef struct nodo *Arbol;
+
+Arbol crearArbol(int x){
+    Arbol p = new Nodo;
+    p->info = x;
+    p->izq = NULL;
+    p->der = NULL;
+    return p;
+}
+
+//------ Agregar nodos a un arbol ------
+void asignarIzq(Arbol a, int unDato){
+    if(a == NULL)
+        cout << "Error: arbol vacio" << endl;
+    else if(a->izq != NULL)
+        cout << "Error: subarbol IZQ ya existe" << endl;
+    else
+        a->izq = crearArbol(unDato);
+}
+
+void asignarDer(Arbol a, int unDato){
+    if(a == NULL)
+        cout << "Error: arbol vacio" << endl;
+    else if(a->der != NULL)
+        cout << "Error: subarbol DER ya existe" << endl;
+    else
+        a->der = crearArbol(unDato);
+}
+
+
+void agregarNodo(Arbol a){
+    //solicitar informacion (numero a agregar)
+    int numero = 0;
+    cout << "Numero a agregar: ";
+    cin >> numero;
+    
+    Arbol p = a;
+    
+    //desplazarse hasta el lugar adecuado
+    while(true){
+        if(numero == p->info){
+            cout << "Error: " << numero << " ya existe" << endl;
+            return;
+        }
+        else if(numero < p->info){//ir a la izquierda
+            if(p->izq == NULL)
+                break;
+            else
+                p = p->izq;
+        }
+        else{//ir a la derecha
+            if(p->der == NULL)
+                break;
+            else
+                p = p->der;
+        }
+    }
+    
+    //agregar el nuevo nodo
+    if(numero < p->info)
+        asignarIzq(p, numero);
+    else
+        asignarDer(p, numero);
+}
+
+//------ Recorrer un arbol (in-orden) ------
+
+void inorden(Arbol a){
+    if(a != NULL){
+        inorden(a->izq);
+        cout << " " << a->info;
+        inorden(a->der);
+    }
+}
+
+void inordenr(Arbol a){
+    if(a != NULL){
+        inordenr(a->der);
+        cout << " " << a->info;
+        inordenr(a->izq);
+    }
+}
+
+
+void recorrerArbol(Arbol a){
+    
+    cout << "Recorrido IN orden:"; inorden(a); cout << endl;
+    cout << "Recorrido IN orden con los sub arboles invertidos: "; inordenr(a); cout << endl;
+   
+}
+
+int main(){
+    int variable = 0;
+    cout<<"Inicializando arbol...\nValor contenido en la raiz: ";
+    cin >> variable;
+    
+    Arbol arbol = crearArbol(variable);
+    
+    bool continuar = true;
+    do{
+        int opcion = 0;
+        cout << "Menu: \n\t1) Agregar\n\t2) Recorrer"
+            << "\n\t3) Salir\n\tOpcion elegida: ";
+        cin >> opcion;
+        switch(opcion){
+            case 1: agregarNodo(arbol);
+            break;
+            case 2: recorrerArbol(arbol);
+            break;
+            case 3: continuar = false;
+            break;
+            default: cout << "Opcion erronea!" << endl;
+            break;
+        }
+    }while(continuar);
+    
+    return 0;
+}
